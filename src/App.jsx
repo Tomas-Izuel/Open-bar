994236bce71db926832ffa77874ef15e471a87ec
Tomas Izuel <tomasizuel@gmail.com>
2022-12-29 15:19:17 -0300

    Navbar y brand terminados en version mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import Navbar from "./components/NavBar/Navbar";
 import "./App.css";
+import logo from './assets/media/Logo.png'
 
 export default function App() {
   return (
     <>
       <header>
-        <Navbar />
+        <a href="" className="flex justify-start items-center gap-4">
+          <img src={logo} alt="" className="brand"/>
+        </a>
       </header>
-      <main>
-      </main>
+      <main></main>
+      <footer>
+        <Navbar />
+      </footer>
     </>
   );
 }
