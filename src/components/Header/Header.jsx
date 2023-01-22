372ef9c4a0b774746b8fc419f5c59012c797d191
Tomas Izuel <tomasizuel@gmail.com>
2023-01-22 14:04:38 -0300

    Preentrega2 Izuel Tomas + cambios

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const a = useParams()
     console.log(a)
   return (
-    <header className=" w-screen flex justify-center items-center pt-5 fixed">
+    <header className=" w-screen flex justify-center items-center p-2 fixed backdrop-blur-sm">
       <Link to="/home">
         <img src={logo} alt="" className=" w-24" />
       </Link>
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 const ItemDetail = ({ product }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1);
 
   const increase = () => {
     setCounter((count) => count + 1);
@@ -10,9 +12,17 @@ const ItemDetail = ({ product }) => {
   const decrease = () => {
     setCounter((count) => count - 1);
   };
+  const addToCart = () => {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Products added successfully',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
   return (
-    <section className="w-screen h-screen flex justify-start items-center pt-24 flex-col gap-10">
-      <h1 className="text-xl">Product detail</h1>
+    <section className="w-screen h-screen flex justify-start items-center py-24 pb-20 flex-col gap-10">
       <div className="flex justify-center w-3/4">
         <div className="rounded-lg shadow-lg bg-white">
           <img className="rounded-t-lg w-full" src={product.image} alt="" />
@@ -22,25 +32,30 @@ const ItemDetail = ({ product }) => {
             </h5>
             <p className="text-gray-800">{product.description}</p>
             <p className="text-gray-700 text-xl mb-4 pt-4">${product.price}</p>
+            <div className="flex gap-3 pb-4">
+              <button className=" border border-slate-800 p-2 w-1/3 text-gray-800 rounded">Suave</button>
+              <button className=" border border-slate-800 p-2 w-1/3 text-gray-800 rounded">Media</button>
+              <button className=" border border-slate-800 p-2 w-1/3 text-gray-800 rounded">Amarga</button>
+            </div>
             <div className="flex w-full justify-center items-center flex-col gap-3">
-              <div className="flex justify-center">
+              <div className="w-full flex justify-center">
                 <button
-                  className=" w-12 h-8 bg-slate-800 rounded-l-lg"
+                  className=" w-1/3 h-8 bg-slate-800 rounded-l-lg"
                   onClick={increase}
                 >
                   +
                 </button>
-                <span className="shadow-inner w-12 h-8 border-1 text-black flex justify-center items-center">
+                <span className="shadow-inner w-1/3 h-8 border-1 text-black flex justify-center items-center">
                   {counter}
                 </span>
                 <button
-                  className=" w-12 h-8 bg-slate-800 rounded-r-lg"
+                  className=" w-1/3 h-8 bg-slate-800 rounded-r-lg"
                   onClick={decrease}
                 >
                   -
                 </button>
               </div>
-              <button className="bg-slate-800 w-3/4 p-1 rounded-lg">
+              <button className=" bg-yellow-400 text-gray-900 w-full p-1 rounded-lg" onClick={addToCart}>
                 Add to cart
               </button>
             </div>
